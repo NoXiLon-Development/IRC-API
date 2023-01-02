35c7523ba92666e78a2ad7ffb5688e0c2ea7c3cf
ccetl <109693935+ccetl@users.noreply.github.com>
2023-01-02 20:58:34 +0100

    python

---
+++ python/main.py
@@ -0,0 +1,55 @@
+from flask import *
+import Key
+
+
+app = Flask(__name__)
+messages = []
+
+
+@app.route('/')
+def route():
+    return 'Hello!'
+
+
+@app.route('/irc/get', methods=['GET'])
+def get():
+    requestKey = request.args.get('key', '')
+    if Key.key != '':
+        if requestKey == '':
+            return 'No permission'
+        if Key.key != requestKey:
+            return 'No permission'
+    else:
+        return jsonify(messages)
+
+
+@app.route('/irc/send', methods=['PUT', 'GET'])
+def put():
+    requestKey = request.args.get('key')
+    message = request.args.get('message')
+    username = request.args.get('username')
+    if Key.key != '':
+        if requestKey == '':
+            return 'No permission'
+        if Key.key != requestKey:
+            return 'No permission'
+    else:
+        if len(message) > 40:
+            message = []
+            message.clear()
+        if message == '' or username == '':
+            return 'Invalid message!'
+        messages.append(Message(message, username))
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
+
+
+class Message:
+    content = ''
+    username = ''
+
+    def __init__(self, content, username):
+        self.content = content
+        self.username = username
